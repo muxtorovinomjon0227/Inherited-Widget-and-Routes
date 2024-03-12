e17b1f640547825bbde0b5fa5a8a8b2c2abc4baf
muxtorovinomjon0227 <muxtorovinomjon0227@gmail.com>
2024-03-12 07:49:21 +0500

    Panda fuu 🐼🐼🐼

---
+++ lib/src/features/dashboard/presentation/view/dashboard_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../../../core/route/route_name.dart';
+import '../../../../core/widgets/inherited_widget.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard Screen",style: Theme.of(context).appBarTheme.titleTextStyle),
+      ),
+      body: _body(context),
+    );
+  }
+  Widget _body(BuildContext context){
+    return TextButton(
+      child: Text("Goto page 2, data=${MyData.of(context).data}"),
+      onPressed: ()=> Navigator.pushNamed(context, RouteName.diagram),
+    );
+  }
+}
---
+++ lib/src/features/diagram/presentation/view/diagram_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../../../core/route/route_name.dart';
+import '../../../../core/widgets/inherited_widget.dart';
+
+class DiagramView extends StatelessWidget {
+  const DiagramView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Diagram Screen",style: Theme.of(context).appBarTheme.titleTextStyle),
+      ),
+      body: _body(context),
+    );
+  }
+  Widget _body(BuildContext context){
+    return TextButton(
+      child: Text("Goto page 2, data=${MyData.of(context).data}"),
+      onPressed: ()=> Navigator.pushReplacementNamed(context, RouteName.home),
+    );
+  }
+}
